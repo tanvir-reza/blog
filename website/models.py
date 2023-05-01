5e5af812c4256a3c5c8d9ffeb4f092f123053307
Md Tanvir Reza <tanvir.reza.abc@gmail.com>
2023-05-02 00:08:14 +0600

    github-desktop

--- website/models.py
+++ website/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    name = models.TextField(max_length=200,blank=True,null=True)
+    name = models.TextField(max_length=250,blank=True,null=True)
     about = models.TextField(blank=True,null=True)
     phone = models.CharField(max_length=20,blank=True,null=True)
     email = models.EmailField(max_length=50, blank=True,null=True)
