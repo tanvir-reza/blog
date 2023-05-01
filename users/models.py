fc105c20ae682fb2cb66267af014b0d09e30b539
KhaledMasud Babu <31078651+imkhaled404@users.noreply.github.com>
2023-05-01 23:52:41 +0600

    model done

--- users/models.py
+++ users/models.py
@@ -44,7 +44,75 @@ class HomeSlider (models.Model):
         super().save(*args, **kwargs)
     def __str__(self):
         return self.title
+ 
+ 
+class AdvisorMsg (models.Model):
+    AdvisorName = models.CharField(max_length=200, blank = True)
+    AdvisorDegi = models.CharField(max_length=200, blank = True)
+    photo = models.ImageField(upload_to='ChairmanMsg/', blank = True)
+    message = RichTextField(blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now = True)
+    created_on = models.DateTimeField(auto_now_add =True)
+    status = models.IntegerField(choices=STATUS, default = 1)
+    total_views=models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Chairman Message'
+        verbose_name_plural = 'Chairman Message'
+
+    #for compress images
+    if photo.blank == False :
+        def save(self, *args, **kwargs):
+
+            # call the compress function
+            new_image = compress(self.photo)
+            # set self.image to new_image
+            self.photo = new_image
+            # save
+            super().save(*args, **kwargs)
+    def __str__(self):
+        return self.AdvisorName
     
+class CollaborationSlider (models.Model):
+    title = models.CharField(max_length=200, blank = True)
+    photo = models.ImageField(upload_to='CollaborationSlider/', blank = False)
+    updated_on = models.DateTimeField(auto_now = True)
+    created_on = models.DateTimeField(auto_now_add =True)
+    status = models.IntegerField(choices=STATUS, default = 1)
+
+       #for compress images
+    def save(self, *args, **kwargs):
+       # call the compress function
+        new_image = compress(self.photo)
+        # set self.image to new_image
+        self.photo = new_image
+        # save
+        super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
+
+class LetestNews(models.Model):
+    title = models.CharField(max_length=200, blank = True)
+    pdf = models.FileField(upload_to='LetestNews/pdf/', blank = True)
+    photo = models.ImageField(upload_to='LetestNews/photo/', blank = True)
+    updated_on = models.DateTimeField(auto_now = True)
+    created_on = models.DateTimeField(blank = False, verbose_name ='On Date')
+    status = models.IntegerField(choices=STATUS, default = 1)
+    total_views=models.IntegerField(default=0)
+    
+    #for compress images
+    if photo.blank == False :
+        def save(self, *args, **kwargs):
+
+            # call the compress function
+            new_image = compress(self.photo)
+            # set self.image to new_image
+            self.photo = new_image
+            # save
+            super().save(*args, **kwargs)
+            
+    def __str__(self):
+        return self.title  
 
 
 class People(models.Model):
@@ -62,4 +130,65 @@ class People(models.Model):
     class Meta:
          verbose_name = "People"
     
+class PublicationCategory(models.Model):
+    PublicationType = models.CharField(max_length= 20, blank = True , default="")
+    class Meta:
+        verbose_name = ("Publication Type")
+        verbose_name_plural = ("Publication Types")
+          
+    def __str__(self):
+        return self.status
+    
+class Publications(models.Model):
+    
+    Publication_Title = models.CharField(max_length=200, blank = False, default="")
+    Author = models.ManyToManyField(People, default="")
+    abstract =  RichTextField(blank=True, null=True , default = "")
+    PublicationCategory = models.ForeignKey(PublicationCategory, verbose_name=("Publication Type"), on_delete=models.CASCADE , default="")
+    DOI = models.CharField(max_length=200, blank = False, default="")
+    status = models.IntegerField(choices=STATUS, default = 1)
+    total_views = models.IntegerField(default=0)
+      
 
+    class Meta:
+        verbose_name = ("Publications")
+        verbose_name_plural = ("Publications")
+
+
+    def __str__(self):
+        return self.project_Title
+    # def get_absolute_url(self):
+    #     return reverse('CapstoneProject:capstoneProject_detail', args=[self.slug])
+
+class ProjectCategory(models.Model):
+    ProjectType = models.CharField(max_length= 20, blank = True , default="")
+    class Meta:
+        verbose_name = ("Projcet Type")
+        verbose_name_plural = ("Projcet Types")
+          
+    def __str__(self):
+        return self.status
+    
+
+class Project(models.Model):
+    
+    project_Title = models.CharField(max_length=200, blank = False, default="")
+    Author = models.ManyToManyField(People, default="")
+    abstract =  RichTextField(blank=True, null=True , default = "")
+    ProjectCategory = models.ForeignKey(ProjectCategory, verbose_name=("Projcet Type"), on_delete=models.CASCADE , default="")
+    Github_Link = models.CharField(max_length=200, blank = False, default="")
+    Funding_agency= models.CharField(max_length=200, blank = False, default="")
+    Funding_period = models.CharField(max_length=200, blank = False, default="")
+    status = models.IntegerField(choices=STATUS, default = 1)
+    total_views = models.IntegerField(default=0)
+      
+
+    class Meta:
+        verbose_name = ("Project")
+        verbose_name_plural = ("Project")
+
+
+    def __str__(self):
+        return self.project_Title
+    # def get_absolute_url(self):
+    #     return reverse('CapstoneProject:capstoneProject_detail', args=[self.slug])
