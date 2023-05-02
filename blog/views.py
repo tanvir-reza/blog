9ebc18dc6a29bc2d91bf741b199ea6db6897953a
KhaledMasud Babu <31078651+imkhaled404@users.noreply.github.com>
2023-05-03 01:35:05 +0600

    comment

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def people(request):
 def publications(request):
     info = Blog.objects.first()
     Authored_books = Publications.objects.filter(category__PublicationType = 'Authored Books')[:10] #when you want to show data from another table you have to use first tablecoloumn then __ then second table coloumn ex here we want to show data from publication table and publicationcategory table so we have to use category__PublicationType
-    #print(Authored_books)
+    #print(Authored_books) #this will print the query set
     
     context  = {"info":info,"Authored_books":Authored_books}
     return render(request,'publications.html',context)
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . import models
 
+# Register your models here.
+admin.site.site_header = "AMIRL ADMIN"
+admin.site.site_title = "@MIRL"
+admin.site.index_title = "API List"
 
 admin.site.register(models.People)
 admin.site.register(models.Publications)
